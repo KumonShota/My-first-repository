3760a90adea1468d2a948caf2b9634d283e83df1
githubのユーザー名 <github>
2025-01-15 06:16:04 +0000

    7-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,10 @@ class PostController extends Controller
     public function index(Post $post)
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
-        //getPagenateByLimit()はPost.phpで定義したメゾット
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
     }
 }
